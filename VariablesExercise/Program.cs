c2c2421551128017a729971068d9be70c75eb9f1
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-10-14 15:56:56 -0400

    added variables and string interpolation

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string showName = "Supernatural";
+
+            int seasons = 16;
+
+            char brothers = '2';
+
+            bool isAlive = true;
+
+            double rating = 6.7D;
+
+            decimal deaths = 500.62M;
+
+            Console.WriteLine($"{showName} is a good tv show, it has {seasons} seasons, {brothers} brothers, their alive status is {isAlive} I give their show rating a {rating} and they have a total for {deaths} deaths.");
         }
     }
 }
